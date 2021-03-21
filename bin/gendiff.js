db850fe914017e59a17ba3e23985d8493dad7e62
Kokova Ekaterina <chibipakost@gmail.com>
2021-03-21 09:07:33 +0700

    edit import commander in bin x2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { Command } from '../node_modules/commander/esm.mjs';
+import { Command } from 'commander/esm.mjs';
 import genDiff from '../src/index.js';
 
 const program = new Command();
