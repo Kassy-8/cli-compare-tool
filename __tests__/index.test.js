42566c87f91f2674f73fef13bfad5fec1b7b662d
Kokova Ekaterina <chibipakost@gmail.com>
2021-03-29 22:37:43 +0700

    change tests according linter rules

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,48 @@
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
-import { test, expect, beforeAll } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const stylishSample = fs.readFileSync(getFixturePath('resultForStylish.txt'), 'utf-8');
+const plainSample = fs.readFileSync(getFixturePath('resultForPlain.txt'), 'utf-8');
+
+const expectedResults = {
+  stylish: stylishSample,
+  plain: plainSample,
+};
+
+test.each([
+  ['file1.json', 'file2.json', 'stylish'],
+  ['file1.yml', 'file2.yml', 'stylish'],
+  ['file1.json', 'file2.json', 'plain'],
+  ['file1.yml', 'file2.yml', 'plain'],
+])('with different formatters', (file1, file2, formatter) => {
+  const rightFile1 = getFixturePath(file1);
+  const rightFile2 = getFixturePath(file2);
+  expect(genDiff(rightFile1, rightFile2, formatter)).toEqual(expectedResults[formatter]);
+});
+
+test.each([
+  ['file1.json', 'file2.json', 'json'],
+  ['file1.yml', 'file2.yml', 'json'],
+])('json output format is valid', (file1, file2, formatter) => {
+  const rightFile1 = getFixturePath(file1);
+  const rightFile2 = getFixturePath(file2);
+  expect(JSON.parse(genDiff(rightFile1, rightFile2, formatter))).toBeTruthy();
+});
+
+test('with default format', () => {
+  const rightFile1 = getFixturePath('file1.json');
+  const rightFile2 = getFixturePath('file2.json');
+  expect(genDiff(rightFile1, rightFile2)).toEqual(expectedResults.stylish);
+});
+/*
+// beforeAll убрано из импортов, чтобы линтер не ругался
 
 const expectedResults = {
   stylish: '',
@@ -32,15 +68,9 @@ beforeAll(() => {
 test.each([
   [0, 'stylish'],
   [1, 'stylish'],
-])('with stylish formatter', (sourceIndex, formatter) => {
-  const [file1, file2] = sourceFiles[sourceIndex];
-  expect(genDiff(file1, file2, formatter)).toEqual(expectedResults[formatter]);
-});
-
-test.each([
   [0, 'plain'],
   [1, 'plain'],
-])('with plain formatter', (sourceIndex, formatter) => {
+])('with stylish formatter', (sourceIndex, formatter) => {
   const [file1, file2] = sourceFiles[sourceIndex];
   expect(genDiff(file1, file2, formatter)).toEqual(expectedResults[formatter]);
 });
@@ -57,3 +87,4 @@ test('with default format', () => {
   const [file1, file2] = sourceFiles[0];
   expect(genDiff(file1, file2)).toEqual(expectedResults.stylish);
 });
+*/
